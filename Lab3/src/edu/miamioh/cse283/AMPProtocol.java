d97751b5e0a6c755f3ec81400eb36d099bcc1d31
Brandon West <bwest1711@gmail.com>
2014-09-12 13:03:00 -0400

    More

--- Lab3/src/edu/miamioh/cse283/AMPProtocol.java
+++ Lab3/src/edu/miamioh/cse283/AMPProtocol.java
@@ -2,4 +2,13 @@ package edu.miamioh.cse283;
 
 public class AMPProtocol {
 
+	public String processInput(String input) {
+		
+		switch(input) {
+		case "" :
+			break;
+		}
+		
+		return null;
+	}
 }
--- Lab3/src/edu/miamioh/cse283/Lab3Client.java
+++ Lab3/src/edu/miamioh/cse283/Lab3Client.java
@@ -1,7 +1,24 @@
 package edu.miamioh.cse283;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Lab3Client {
 	public static void main(String[] args) {
-		
+		String _address = args[0];
+		String _port = args[0];
+		int port = Integer.parseInt(_port);
+
+		try (
+				Socket s = new Socket(_address, port);
+				PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+				BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));) {
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
