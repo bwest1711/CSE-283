d8a9bd3c2b2eb590a284470cbc067f2f6bc3982e
Brandon West <bwest1711@gmail.com>
2014-09-12 13:16:24 -0400

    switch for protocol

--- Lab3/src/edu/miamioh/cse283/AMPProtocol.java
+++ Lab3/src/edu/miamioh/cse283/AMPProtocol.java
@@ -8,6 +8,24 @@ public class AMPProtocol {
 		case "":
 			toReturn = "This is a test";
 			break;
+		case "GET WORK":
+			toReturn = "This is a test";
+			break;
+		case "PUT ANSWER":
+			toReturn = "This is a test";
+			break;
+		case "AMP WORK":
+			toReturn = "This is a test";
+			break;
+		case "AMP NONE":
+			toReturn = "This is a test";
+			break;
+		case "AMP OK":
+			toReturn = "This is a test";
+			break;
+		case "AMP ERROR":
+			toReturn = "This is a test";
+			break;
 		}
 
 		return toReturn;
