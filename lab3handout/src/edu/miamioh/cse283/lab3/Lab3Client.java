fe083c59b70da2697e55133002fd99a5a67e93d2
Brandon <bwest1711@gmail.com>
2014-09-17 18:24:54 -0400

    done?

--- lab3handout/src/edu/miamioh/cse283/lab3/Lab3Client.java
+++ lab3handout/src/edu/miamioh/cse283/lab3/Lab3Client.java
@@ -1,15 +1,15 @@
 package edu.miamioh.cse283.lab3;
 
-import java.io.*;
-import java.net.*;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 /**
  * Template client for CSE283 Lab 3, FS2014.
  * 
- * This client should read the following from the command line:
- * 1) remote address of the server 
- * 2) remote port of the server
+ * This client should read the following from the command line: 1) remote address of the server 2) remote port of the server
  * 
  * This client should process all available work on the server and then terminate.
  * 
@@ -22,12 +22,14 @@ public class Lab3Client {
 	public static final String AMP_NONE = "AMP NONE";
 	public static final String AMP_OK = "AMP OK";
 	public static final String AMP_ERROR = "AMP ERROR";
-	
+
 	/**
 	 * Runs the Lab3Client.
 	 * 
-	 * @param args is an array containing each of the command-line arguments.
-	 * @throws IOException if there is a networking error.
+	 * @param args
+	 *            is an array containing each of the command-line arguments.
+	 * @throws IOException
+	 *             if there is a networking error.
 	 */
 	public static void main(String[] args) throws IOException {
 		if (args.length != 2) {
@@ -35,63 +37,68 @@ public class Lab3Client {
 			return;
 		}
 
-		Socket client=null;
-		
+		Socket client = null;
+
 		try {
 			// Construct a socket and connect it to the remote address and port given by the command line arguments:
 			// client = new Socket...
+			client = new Socket(args[0], Integer.parseInt(args[1]));
 			System.out.println("CONNECTED TO: " + client.getRemoteSocketAddress());
-			
-			// Build the writer & reader from the client's socket input and output streams: 
-			PrintWriter out=null;
-			BufferedReader in=null;
 
-			String response=null;
+			// Build the writer & reader from the client's socket input and output streams:
+			PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+			String response = null;
 			do {
 				System.out.println("REQUESTING WORK");
 				// send a get work message to the server (use "out" from above):
-				
+				out.println(GET_WORK);
 				// read its response (use "in" from above):
-				// response = ...
-				
+				response = in.readLine();
+
 				// if the response includes work:
-				if(response.startsWith(AMP_WORK)) {
+				if (response.startsWith(AMP_WORK)) {
 					// read the next line (which will contain the math problem):
-					// response = ...
+					response = in.readLine();
 					System.out.println("  WORK: " + response);
 
 					// solve the math problem (see MathProblem.solve, in this package)
-					Double ans=null;
-					
+					Double ans = MathProblem.solve(response);
+
 					// if ans is null, then the math problem was malformed.
 					// you can choose how to handle this case, but make sure that
 					// you don't break the server.
-					if(ans == null) {
+					if (ans == null) {
 						ans = 0.0;
 					}
-										
+
 					// send the answer to the server (don't forget the header, use "out"):
+					out.println("PUT ANSWER");
+					out.println(ans);
 					System.out.println("  SENDING ANSWER: " + ans);
-					
+
 					// the server should respond with OK if everything went well
-					// note: OK does not mean the answer is right, just that the 
+					// note: OK does not mean the answer is right, just that the
 					// server understood the message.
-					
+
 					// receive the response (use "in"):
-					// response = ...
-					if(response.startsWith(AMP_OK)) {
+					response = in.readLine();
+					if (response.startsWith(AMP_OK)) {
 						System.out.println("  ANSWER RECEIVED");
 					} else {
 						System.out.println("  ANSWER MALFORMED");
 					}
 				}
-			} while(!response.startsWith(AMP_NONE)); // loop until an AMP_NONE response is received
-			
+			} while (!response.startsWith(AMP_NONE)); // loop until an AMP_NONE response is received
+
 			System.out.println("NO WORK AVAILABLE; TERMINATING");
 
+		} catch (Exception e) {
+			e.printStackTrace();
 		} finally {
 			// close the socket:
-			if(client != null) {
+			if (client != null) {
 				client.close();
 			}
 		}
--- lab3handout/src/edu/miamioh/cse283/lab3/Lab3Server.java
+++ lab3handout/src/edu/miamioh/cse283/lab3/Lab3Server.java
@@ -1,19 +1,20 @@
 package edu.miamioh.cse283.lab3;
 
-import java.io.*;
-import java.net.*;
-import java.util.*;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
 
 /**
  * Template server for CSE283 Lab 3, FS2014.
  * 
- * This server should read the following from the command line:
- * 1) local port on which to listen for client connections 
- * 2) number of work items to distribute to each client (nwork)
+ * This server should read the following from the command line: 1) local port on which to listen for client connections 2) number of work items to distribute to each client (nwork)
  * 
- * This server should respond with nwork randomly-generated simple math problems,
- * and then allow the client to terminate.
+ * This server should respond with nwork randomly-generated simple math problems, and then allow the client to terminate.
  * 
  * @author dk
  */
@@ -24,90 +25,97 @@ public class Lab3Server {
 	public static final String AMP_NONE = "AMP NONE";
 	public static final String AMP_OK = "AMP OK";
 	public static final String AMP_ERROR = "AMP ERROR";
-	
+
 	public static void main(String[] args) throws IOException {
 		if (args.length != 2) {
 			System.out.println("Usage: java Lab3Server <port number> <n work items>");
 			return;
 		}
-		
-		ServerSocket server=null;
-		Socket client=null;
+
+		ServerSocket server = null;
+		Socket client = null;
 		int nwork = Integer.parseInt(args[1]);
-		
+
 		try {
+
+			server = new ServerSocket(10780);
+			client = server.accept();
 			// construct a server socket:
 			// server = new ServerSocket...
 			System.out.println("Lab3Server listening on: " + server.getLocalSocketAddress());
 			System.out.println("Lab3Server listening on: " + InetAddress.getLocalHost().getHostAddress() + ":" + server.getLocalPort());
 
 			// loop "forever":
-			while(true) {
+			while (true) {
 				// accept a client connection from the server socket:
-				// client = ... 
-				
+				// client = ...
+
 				// and build the reader and writer:
-				PrintWriter out=null;
-				BufferedReader in=null;
+				PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+				BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
 
 				String line;
-				Double expectedAns=0.0;
-				int count=0;
-				int correct=0;
-				
+				Double expectedAns = 0.0;
+				int count = 0;
+				int correct = 0;
+
 				// loop while we are able to read lines from the client
 				// (this is a safeguard against a client that terminates early):
-				while((line = in.readLine()) != null) {
+				while ((line = in.readLine()) != null) {
 					System.out.println("CLIENT REQUEST: " + line);
-					
+
 					// if the client is requesting work:
-					if(line.startsWith(GET_WORK)) {
+					if (line.startsWith(GET_WORK)) {
 						// if we have more work to do:
-						if(count < nwork) {
+						if (count < nwork) {
 							// generate a math problem, and calculate the expected answer
 							// (see MathProblem.generate and MathProblem.solve, in this package)
-							String amp=null;
-							expectedAns=null;
-							
+							String amp = MathProblem.generate();
+							expectedAns = MathProblem.solve(amp);
+
 							// send the work response to the client (use "out", don't forget the header):
+							out.println(AMP_WORK);
+							out.println(amp);
 							System.out.println("  RESPONSE: " + amp);
 							++count;
 						} else { // all done; tell the client we're out of work:
 							System.out.println("  RESPONSE: NONE");
-							// send the "none" response to the client
+							out.println(AMP_NONE);
 							break;
 						}
-					} else if(line.startsWith(PUT_ANSWER)) { // client is sending an answer:
+					} else if (line.startsWith(PUT_ANSWER)) { // client is sending an answer:
 						// read the client's answer (use "in"):
-						String answer=null;
-						
+						String answer = in.readLine();
+						System.out.println("  ANSWER: " + answer);
 						// if the client's answer matches our expected answer:
-						if(expectedAns == Double.parseDouble(answer)) {
+						if (expectedAns == Double.parseDouble(answer)) {
 							System.out.println("  CORRECT");
 							++correct;
 						} else {
 							System.out.println("  INCORRECT");
 						}
 						System.out.println("  RESPONSE: OK");
-						
+
 						// respond with OK (use "out"):
+						out.println(AMP_OK);
 					} else {
 						// garbled input from the client; respond with AMP_ERROR (use "out"):
+						out.println(AMP_ERROR);
 						System.out.println("  RESPONSE: ERROR");
 					}
 				}
-				
+
 				client.close();
 				System.out.println("---- END: " + correct + " OF " + nwork + " CORRECT RESPONSES ----");
 			}
-		} catch(SocketException ex) {
+		} catch (SocketException ex) {
 			// only get here if something went wrong
 			System.out.println("EXCEPTION: " + ex.getMessage());
 		} finally {
-			if(server != null) {
+			if (server != null) {
 				server.close();
 			}
-			if( client != null) {
+			if (client != null) {
 				client.close();
 			}
 		}
