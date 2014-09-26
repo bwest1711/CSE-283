fd3d1d739313ffa2ea77d53314fd0974f369659d
Brandon West <bwest1711@gmail.com>
2014-09-26 12:58:33 -0400

    moving to lab4

--- concurrencyLab/src/concurrencyLab/ThreadTest.java
+++ concurrencyLab/src/concurrencyLab/ThreadTest.java
@@ -1,22 +1,55 @@
 package concurrencyLab;
 
+import java.util.ArrayList;
+
 public class ThreadTest {
 
 	public class SmallThread implements Runnable {
-		public SmallThread() {
+		int index;
+		ThreadTest server;
 
+		public SmallThread(int i, ThreadTest server) {
+			index = i;
+			this.server = server;
 		}
 
 		@Override
 		public void run() {
-
+			synchronized (server) {
+				int c = server.getCounter();
+				System.out.println("Hello world " + index);
+				System.out.println("  counter value: " + c);
+				server.putCounter(c + 1);
+			}
 		}
 	}
 
+	protected int counter;
+
+	public void putCounter(int c) {
+		counter = c;
+	}
+
+	public int getCounter() {
+		return counter;
+	}
+
 	public void serve(String[] args) {
-		System.out.println("Hello world");
-		SmallThread t = new SmallThread();
-		
+		ArrayList<Thread> list = new ArrayList<Thread>();
+		for (int i = 0; i < 100; i++) {
+			SmallThread t = new SmallThread(i, this);
+			Thread thread = new Thread(t);
+			list.add(thread);
+			thread.start();
+		}
+
+		for (Thread t : list) {
+			try {
+				t.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	public static void main(String[] args) {
