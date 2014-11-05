1fb9a32055b34b082987570196a98e89935e5179
westbs2 <westbs2@EAS-BEN006-B1.it.muohio.edu>
2014-11-05 13:39:49 -0500

    Client structure.

--- Hunt the Wumpus/src/edu/miamioh/cse283/wumpus/net/Client.java
+++ Hunt the Wumpus/src/edu/miamioh/cse283/wumpus/net/Client.java
@@ -1,13 +1,40 @@
 package edu.miamioh.cse283.wumpus.net;
 
+import java.net.InetAddress;
+
 public class Client {
 	/**
-	 * Networking code:
+	 * Plays the game
 	 * 
-	 * connecting
-	 * handoff
-	 * 
-	 * send actions --> server
-	 * recv messages <-- server
+	 * @param args - Holds the address and port number for 
+	 * the CaveSystemServer this client will connect to
 	 */
+	public void run(String[] args){
+		
+	}
+	
+	/**
+	 * Connect this client to the server.
+	 * 
+	 * @param addr is the address for the server.
+	 * @param port is the port number for the server.
+	 */
+	public void connect(InetAddress addr, int port){
+		
+	}
+
+	/**
+	 * Hands off the client to a new (different) server.
+	 * 
+	 * @param addr is the address for the server.
+	 * @param port is the port number for the server.
+	 */
+	public void handoff(InetAddress addr, int port){
+		
+	}
+	
+	public static void main(String[] args) {
+		Client c = new Client();
+		c.run(args);
+	}
 }
