92eec6e257a188e5b2a793cb3b3dfe3f883474ae
Brandon West <bwest1711@gmail.com>
2014-11-07 12:35:12 -0500

    Added multi-threading to CaveSystemServer

--- Hunt the Wumpus/src/edu/miamioh/cse283/wumpus/net/CaveSystemServer.java
+++ Hunt the Wumpus/src/edu/miamioh/cse283/wumpus/net/CaveSystemServer.java
@@ -6,13 +6,37 @@ import java.net.Socket;
 
 public class CaveSystemServer {
 
+	/** Inner class to handle multiple threads */
+	public class ServerThread implements Runnable{
+		protected Socket client;
+		
+		public ServerThread(Socket client) {
+			this.client = client;
+		}
+		
+		@Override
+		public void run() {
+			
+		}
+	}
+	
 	protected ServerSocket clientSocket;
 
+	/**
+	 * Run from a non static context.
+	 * 
+	 * @param portBase
+	 * @throws IOException temporary until we change to try-catch
+	 */
 	public void run(int portBase) throws IOException {
 		clientSocket = new ServerSocket(portBase);
 		
 		while(true){
 			Socket client = clientSocket.accept();
+			
+			// Create a new thread and pass our client
+			(new Thread(new ServerThread(client))).start();
+			
 		}
 	}
 
--- Hunt the Wumpus/src/edu/miamioh/cse283/wumpus/net/Client.java
+++ Hunt the Wumpus/src/edu/miamioh/cse283/wumpus/net/Client.java
@@ -5,43 +5,50 @@ import java.net.InetAddress;
 import java.net.Socket;
 
 public class Client {
-	
+
+	/** Socket for communication */
 	protected Socket server;
-	
+
 	/**
 	 * Runs the game
-	 * @param addr the ip address of the server
-	 * @param port the port number of the server
-	 * @throws IOException 
+	 * 
+	 * @param addr
+	 *            the ip address of the server
+	 * @param port
+	 *            the port number of the server
+	 * @throws IOException
 	 */
-	public void run(InetAddress addr, int port) throws IOException{
+	public void run(InetAddress addr, int port) throws IOException {
 		server = new Socket(addr, port);
-		System.out.println("Something happened.");		
+		System.out.println("Something happened.");
 	}
-	
+
 	/**
 	 * Connect this client to the server.
 	 * 
-	 * @param addr is the address for the server.
-	 * @param port is the port number for the server.
+	 * @param addr
+	 *            is the address for the server.
+	 * @param port
+	 *            is the port number for the server.
 	 */
-	public void connect(InetAddress addr, int port){
-		
+	public void connect(InetAddress addr, int port) {
+
 	}
 
 	/**
 	 * Hands off the client to a new (different) server.
 	 * 
-	 * @param addr is the address for the server.
-	 * @param port is the port number for the server.
+	 * @param addr
+	 *            is the address for the server.
+	 * @param port
+	 *            is the port number for the server.
 	 */
-	public void handoff(InetAddress addr, int port){
-		
+	public void handoff(InetAddress addr, int port) {
+
 	}
-	
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		Client c = new Client();
-		c.run(InetAddress.getByName(args[0]),
-				Integer.parseInt(args[1]));
+		c.run(InetAddress.getByName(args[0]), Integer.parseInt(args[1]));
 	}
 }
