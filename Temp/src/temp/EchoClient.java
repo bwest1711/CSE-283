4d4fda21f812b7881d3dc3c1209f22e6674ae4ae
westbs2 <westbs2@EAS-BEN006-B4.it.muohio.edu>
2014-11-12 12:27:35 -0500

    Moved to inner proxy class

--- Temp/src/temp/EchoClient.java
+++ Temp/src/temp/EchoClient.java
@@ -1,5 +1,7 @@
 package temp;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
@@ -7,8 +9,10 @@ import java.net.Socket;
 public class EchoClient {
 	public static void main(String[] args) throws Exception {
 		Socket s = new Socket(InetAddress.getByName(args[0]), Integer.parseInt(args[1]));
+		BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
 		PrintWriter out = new PrintWriter(s.getOutputStream(), true);
-		
+
 		out.println("hello world");
+		System.out.println(in.readLine());
 	}
 }
--- Temp/src/temp/EchoServer.java
+++ Temp/src/temp/EchoServer.java
@@ -2,17 +2,35 @@ package temp;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
+	public static class ClientProxy {
+		Socket s;
+		BufferedReader in;
+		PrintWriter out;
+
+		public ClientProxy(Socket s) throws Exception {
+			this.s = s;
+			in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+			out = new PrintWriter(s.getOutputStream(), true);
+		}
+
+		public void echo() throws Exception {
+			String message = in.readLine();
+			System.out.println(message);
+			out.println(message);
+		}
+	}
+
 	public static void main(String[] args) throws Exception {
 		ServerSocket s = new ServerSocket(Integer.parseInt(args[0]));
 		while (true) {
-			Socket client = s.accept();
+			ClientProxy cp = new ClientProxy(s.accept());
 
-			BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-			System.out.println(in.readLine());
+			cp.echo();
 		}
 	}
 }
