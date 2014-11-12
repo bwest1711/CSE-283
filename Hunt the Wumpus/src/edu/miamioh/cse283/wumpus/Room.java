d828501d8e1c4919c3cfa16d88864fc427e2691d
westbs2 <westbs2@EAS-BEN006-B4.it.muohio.edu>
2014-11-12 12:44:39 -0500

    Updated with code from class.

--- Hunt the Wumpus/src/edu/miamioh/cse283/wumpus/Room.java
+++ Hunt the Wumpus/src/edu/miamioh/cse283/wumpus/Room.java
@@ -1,49 +1,14 @@
 package edu.miamioh.cse283.wumpus;
 
+import java.util.ArrayList;
+
+import edu.miamioh.cse283.wumpus.net.proxy.ClientProxy;
+
 public class Room {
-	private final int id;
-	private boolean hasGold, hasWumpus, hasPlayer;
-	private final boolean hasBats, hasPit;
-
-	public Room(int id, boolean hasBats, boolean hasPit) {
-		this.id = id;
-		this.hasBats = hasBats;
-		this.hasPit = hasPit;
+	public ArrayList<ClientProxy> players;
+	
+	public Room() {
+		players = new ArrayList<ClientProxy>();
 	}
-
-	public boolean hasGold() {
-		return hasGold;
-	}
-
-	public void setHasGold(boolean hasGold) {
-		this.hasGold = hasGold;
-	}
-
-	public boolean hasWumpus() {
-		return hasWumpus;
-	}
-
-	public void setHasWumpus(boolean hasWumpus) {
-		this.hasWumpus = hasWumpus;
-	}
-
-	public boolean hasPlayer() {
-		return hasPlayer;
-	}
-
-	public void setHasPlayer(boolean hasPlayer) {
-		this.hasPlayer = hasPlayer;
-	}
-
-	public boolean hasBats() {
-		return hasBats;
-	}
-
-	public boolean hasPit() {
-		return hasPit;
-	}
-
-	public int getId() {
-		return id;
-	}
+	
 }
--- Hunt the Wumpus/src/edu/miamioh/cse283/wumpus/net/CaveServer.java
+++ Hunt the Wumpus/src/edu/miamioh/cse283/wumpus/net/CaveServer.java
@@ -1,140 +1,112 @@
 package edu.miamioh.cse283.wumpus.net;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 
 import edu.miamioh.cse283.wumpus.Room;
+import edu.miamioh.cse283.wumpus.net.proxy.CaveSystemServerProxy;
+import edu.miamioh.cse283.wumpus.net.proxy.ClientProxy;
 
 public class CaveServer {
-	/** Inner class to handle multiple threads */
-	public class CaveThread implements Runnable {
 
-		/** The list of rooms */
-		protected ArrayList<Room> rooms;
+	/** Port base for this cave server. */
+	protected int portBase;
 
-		/** Connection to the player */
-		protected Socket client;
-
-		/** The message that is displayed when a player enters the cave */
-		private final String startMessage = "   _____  _                 _                   _                    _            _                    _ \n"
-				+ "  |_   _|| |_   __ _  _ _  | |__  __ _  ___  __| |  _  _  ___  _  _ ( )_ _  ___  | |_   ___  _ _  ___ | |\n"
-				+ "    | |  | ' \\ / _` || ' \\ | / / / _` |/ _ \\/ _` | | || |/ _ \\| || ||/| '_|/ -_) | ' \\ / -_)| '_|/ -_)|_|\n"
-				+ "    |_|  |_||_|\\__,_||_||_||_\\_\\ \\__, |\\___/\\__,_|  \\_, |\\___/ \\_,_|  |_|  \\___| |_||_|\\___||_|  \\___|(_)\n"
-				+ "                                 |___/              |__/                                                 \n\n" + " SHOOT - shoots an arrow\n"
-				+ "PICKUP - picks up whatever gold/arrows are on the ground\n" + " CLIMB - climbs the ladder to exit the cave\n"
-				+ "  MOVE - followed by a number to move to a different room\n";
-
-		/**
-		 * Creates a new instance of the cave system.
-		 * 
-		 * @param client
-		 *            the socket connected to the client
-		 */
-		public CaveThread(Socket client) {
-			this.client = client;
-			init();
-		}
-
-		private void init() {
-			rooms = new ArrayList<Room>();
-			for(int i = 0; i < 20; i++){
-				Room temp = new Room(i, false, false);
-				rooms.add(temp);
-			}
-		}
-
-		/**
-		 * Determine how many lines a string contains.
-		 * 
-		 * @param message
-		 * @return number of lines
-		 */
-		private int getNumLines(String message) {
-			char[] c = message.toCharArray();
-			int toReturn = 1;
-
-			for (char letter : c) {
-				if (letter == '\n')
-					toReturn++;
-			}
-
-			return toReturn;
-		}
-
-		@Override
-		public void run() {
-			try {
-				BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-				PrintWriter out = new PrintWriter(client.getOutputStream(), true);
-
-				out.println(getNumLines(startMessage));
-				out.println(startMessage);
-
-				String input;
-
-				// Loop for input from user
-				while ((input = in.readLine()) != null) {
-					System.out.println("[Client] " + input);
-					out.println(1);
-					switch (input.toUpperCase().trim()) {
-					case "SHOOT":
-						out.println("You shot me!"); // This can become Player.shoot(direction)
-						break;
-					case "PICKUP":
-						out.println("You found gold!"); // Player.pickup();
-						break;
-					case "MOVE":
-						out.println("You moved somewhere!"); // Player.move(direction)
-						break;
-					case "CLIMB":
-						out.println("You climbed stuff!"); // Player.climb()
-						break;
-					default:
-						out.println("Not a valid command!");
-						break;
-					}
-				}
-
-			} catch (IOException e) {
-				System.err.println(e.getMessage());
-				//e.printStackTrace();
-			}
-		}
-	}
-
-	/** Listening Socket for new client connections */
+	/** Socket for accepting connections from players. */
 	protected ServerSocket clientSocket;
 
-	/**
-	 * Run from a non static context.
-	 * 
-	 * @param portBase
-	 * @throws IOException
-	 *             temporary until we change to try-catch
-	 */
-	public void run(int portBase) throws IOException {
-		// Listen on the base port
-		clientSocket = new ServerSocket(portBase);
+	/** Proxy to the CaveSystemServer. */
+	protected CaveSystemServerProxy caveSystem;
 
-		while (true) {
-			// Always wait for something to try to connect
-			Socket client = clientSocket.accept();
+	private final String startMessage = "   _____  _                 _                   _                    _            _                    _ \n"
+			+ "  |_   _|| |_   __ _  _ _  | |__  __ _  ___  __| |  _  _  ___  _  _ ( )_ _  ___  | |_   ___  _ _  ___ | |\n"
+			+ "    | |  | ' \\ / _` || ' \\ | / / / _` |/ _ \\/ _` | | || |/ _ \\| || ||/| '_|/ -_) | ' \\ / -_)| '_|/ -_)|_|\n"
+			+ "    |_|  |_||_|\\__,_||_||_||_\\_\\ \\__, |\\___/\\__,_|  \\_, |\\___/ \\_,_|  |_|  \\___| |_||_|\\___||_|  \\___|(_)\n"
+			+ "                                 |___/              |__/                                                 \n\n"
+			+ " SHOOT - shoots an arrow\n" + "PICKUP - picks up whatever gold/arrows are on the ground\n"
+			+ " CLIMB - climbs the ladder to exit the cave\n" + "  MOVE - followed by a number to move to a different room\n";
 
-			// Create a new thread and pass our client
-			(new Thread(new CaveThread(client))).start();
+	/** Rooms in this CaveServer. */
+	protected ArrayList<Room> rooms;
 
+	/** Constructor. */
+	public CaveServer(CaveSystemServerProxy caveSystem, int portBase) {
+		this.caveSystem = caveSystem;
+		this.portBase = portBase;
+		rooms = new ArrayList<Room>();
+		for (int i = 0; i < 20; ++i) {
+			rooms.add(new Room());
 		}
 	}
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		System.out.println("Starting CaveServer");
-		System.out.println("  CaveServer:  args[0] = " + args[0]);
-		CaveServer s = new CaveServer();
-		s.run(Integer.parseInt(args[0]));
+	/** Returns the port number to use for accepting client connections. */
+	public int getClientPort() {
+		return portBase;
+	}
+
+	/** This is the thread that handles a single client connection. */
+	public class ClientThread implements Runnable {
+		/** This is our "client" (actually, a proxy to the network-connected client). */
+		protected ClientProxy client;
+
+		/** Constructor. */
+		public ClientThread(ClientProxy client) {
+			this.client = client;
+		}
+
+		/**
+		 * Play the game with this client.
+		 */
+		public void run() {
+			try {
+				client.message("Welcome!");
+
+				// put the player in a room (any room is fine)
+
+				// now, in a loop while the player is alive:
+				// -- send the player their "senses":
+				client.senses("You are in an empty room.");
+				// -- and retrieve their action:
+				String action = client.getAction();
+				// -- and perform the action
+
+			} catch (Exception ex) {
+				// If an exception is thrown, we can't fix it here -- Crash.
+				ex.printStackTrace();
+				System.exit(1);
+			}
+		}
+	}
+
+	/** Runs the CaveSystemServer. */
+	public void run() {
+		try {
+			clientSocket = new ServerSocket(getClientPort());
+			caveSystem.register(clientSocket);
+
+			while (true) {
+				// and now loop forever, accepting client connections:
+				while (true) {
+					ClientProxy client = new ClientProxy(clientSocket.accept());
+					(new Thread(new ClientThread(client))).start();
+				}
+			}
+		} catch (Exception ex) {
+			// If an exception is thrown, we can't fix it here -- Crash.
+			ex.printStackTrace();
+			System.exit(1);
+		}
+	}
+
+	/** Main method (run the CaveServer). */
+	public static void main(String[] args) throws Exception {
+		// first, we need our proxy object to the CaveSystemServer:
+		CaveSystemServerProxy caveSystem = new CaveSystemServerProxy(new Socket(InetAddress.getByName(args[0]), Integer.parseInt(args[1]) + 1));
+
+		// now construct this cave server, and run it:
+		CaveServer cs = new CaveServer(caveSystem, Integer.parseInt(args[2]));
+		cs.run();
 	}
 }
