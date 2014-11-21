bfea0b45787fc3ef9a8d980a195b2f3cce0ba4ab
Brandon <bwest1711@gmail.com>
2014-11-21 16:52:03 -0500

    stuff

--- Hunt the Wumpus/src/edu/miamioh/cse283/wumpus/Room.java
+++ Hunt the Wumpus/src/edu/miamioh/cse283/wumpus/Room.java
@@ -6,9 +6,30 @@ import edu.miamioh.cse283.wumpus.net.proxy.ClientProxy;
 
 public class Room {
 	public ArrayList<ClientProxy> players;
-	
-	public Room() {
+	private final int roomNum, roomOne, roomTwo, roomThree;
+	private final String danger;
+
+	public Room(String roomNum, String roomOne, String roomTwo, String roomThree, String danger) {
 		players = new ArrayList<ClientProxy>();
+
+		// Accidentally built the rooms based 1...instead of redoing all the typing
+		this.roomNum = Integer.parseInt(roomNum) - 1;
+		this.roomOne = Integer.parseInt(roomOne) - 1;
+		this.roomTwo = Integer.parseInt(roomTwo) - 1;
+		this.roomThree = Integer.parseInt(roomThree) - 1;
+		this.danger = danger;
 	}
-	
+
+	public String getPath() {
+		return "You are in room " + roomNum + " there are paths to rooms " + roomOne + ", " + roomTwo + ", and " + roomThree;
+	}
+
+	public int[] getNeighbors() {
+		int[] toReturn = { roomOne, roomTwo, roomThree };
+		return toReturn;
+	}
+
+	public String sense() {
+		return danger;
+	}
 }
